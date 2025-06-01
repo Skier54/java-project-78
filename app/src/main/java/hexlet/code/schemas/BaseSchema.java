30dd9a67037d29760d637595d2a74c6267150620
Pavel Tretyakov <tretyakovpavel54@mail.ru>
2025-06-01 23:38:27 +0700

    modified BaseSchema

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -1,5 +1,11 @@
 package hexlet.code.schemas;
 
+/**
+ * Базовый класс для схем валидации.
+ * Представляет собой основу для создания различных схем валидации данных.
+ *
+ * @param <T> тип данных, которые будут валидироваться
+ */
 public abstract class BaseSchema<T> {
     protected boolean required;
 
@@ -9,11 +15,26 @@ public abstract class BaseSchema<T> {
 
     public abstract boolean isValid(T value);
 
+    /**
+     * Устанавливает требование обязательности поля.
+     * При переопределении метода необходимо учитывать,
+     * что поле required должно быть корректно инициализировано.
+     *
+     * @return текущий экземпляр схемы
+     */
     public BaseSchema<T> required() {
         this.required = true;
         return this;
     }
 
+    /**
+     * Выполняет базовую проверку значения на обязательность.
+     * При переопределении метода необходимо учитывать,
+     * что проверка required должна быть выполнена.
+     *
+     * @param value проверяемое значение
+     * @return true если значение проходит базовую проверку
+     */
     public boolean isValidCommon(T value) {
         return required && value == null;
     }
